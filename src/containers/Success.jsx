fe9f873fd8bf744455dfa8484e5758dd9babe958
Beto <betoml5@hotmail.com>
2021-07-14 11:51:17 -0500

    First commit

---
+++ src/containers/Success.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/components/Success.css';
+
+const Success = () => (
+        <h1>Success</h1>
+    )
+
+export default Success
---
+++ src/routes/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from '../containers/Home';
+import Information from '../containers/Information';
+import Checkout from '../containers/Checkout';
+import Payment from '../containers/Payment';
+import Success from '../containers/Success';
+import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout';
+
+const App = () => (
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/checkout/information" component={Information} />
+        <Route exact path="/checkout/success" component={Payment} />
+        <Route exact path="/checkout/payment" component={Success} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
+);
+
+export default App;
